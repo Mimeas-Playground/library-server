6a39a1e926e4575316e107f5a0c3399041482eea
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2022-10-13 16:02:32 +0200

    Added a get book api endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
-use rocket::{Error, fs::{FileServer, relative}};
+use rocket::{Error, fs::{FileServer, relative}, serde::{self, ser::SerializeStruct}};
 
-#[rocket::main]
+#[macro_use] extern crate rocket;
+
+#[main]
 async fn main() -> Result<(), Error>{
     let _ = rocket::build()
     .mount("/", FileServer::from(relative!("web")))
+    .mount("/api", routes![api::get_books])
     .launch().await?;
 
     Ok(())
+}
+
+mod api {
+    use super::Book;
+    use rocket::{serde::json::Json, get};
+
+    #[get("/")]
+    pub fn get_books() -> Json<Vec<Book>> {
+        let book_list = vec![Book{title: String::from("A story")}];
+
+        Json(book_list)
+    }
+}
+
+pub struct Book {
+    pub title: String
+}
+
+impl serde::Serialize for Book {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer {
+        let mut s =serializer.serialize_struct("Book", 1)?;
+        s.serialize_field("title", &self.title)?;
+        s.end()
+    }
 }
