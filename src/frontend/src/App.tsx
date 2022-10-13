ff3cd6725472cd941b78088cd3030e387b05916c
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2022-10-13 16:36:52 +0200

    frontend can now list books

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,11 +1,40 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
+  return (<>
+    <h1>SHHH!!!</h1>
+    <h2>This is a library</h2>
+    <hr/>
+    <BookList/>
+    </>)
+}
+
+function BookList() {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    fetch("/api")
+      .then(async (res) => {
+        setBooks(await res.json())
+      })
+  }, [])
+
   return (
-    <h1>Hello World!</h1>
+    <ul className="book">
+      {
+        books.map(
+          (b: Book) => {
+            return <h3>{b.title}</h3>
+          }
+        )
+      }
+    </ul>
   )
+}
+
+interface Book {
+  title: string
 }
 
 export default App
