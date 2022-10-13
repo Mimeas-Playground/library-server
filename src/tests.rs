30c070a21947604e5842ccb5cbf496dc828fce9d
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2022-10-13 17:29:30 +0200

    💙 made get test more specific ❤ post book (404)

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,9 @@
 use rocket::local::blocking::Client;
+use crate::Book;
+
 mod api {
     use rocket::{http::Status};
+
     use super::*;
 
     #[test]
@@ -9,10 +12,26 @@ mod api {
         let response = client.get("/api").dispatch();
         assert_eq!(response.status(), Status::Ok);
 
-        let data = response.into_string();
-        if let Some(dat) = data {
-            assert!(dat.contains("\"title\":\"A story\""))
+        if let Some(data) = response.into_json::<Vec<Book>>() {
+            assert!(data.contains(&Book{title:"A story".to_string()}))
         }
         else {panic!("response returned nothing")}
+    }
+
+    #[test]
+    fn should_post_book() {
+        let book = Book {title:  String::from("A Post")};
+
+        let client = Client::tracked(crate::rocket()).unwrap();
+        let post_response = client.post("/api")
+            .json(&book)
+            .dispatch();
+        assert_eq!(post_response.status(), Status::Ok);
+
+        let get_response = client.get("/api").dispatch();
+        assert_eq!(get_response.status(), Status::Ok);
+        if let Some(data) = get_response.into_json::<Vec<Book>>() {
+            assert!(data.contains(&book))
+        } else {panic!("Got no data")}
     }
 }
