7109e28c880b8acdfab74f7844da086051d82a15
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2022-10-14 15:10:57 +0200

    💙 Added a rocket configuration

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate lazy_static;
 
-use rocket::{fs::{FileServer, relative}, serde::{Serialize, Deserialize}};
+use std::net::{Ipv4Addr, IpAddr};
+use rocket::{fs::{FileServer, relative}, serde::{Serialize, Deserialize}, Config};
 
 #[cfg(test)]
 mod tests;
@@ -10,10 +11,26 @@ mod data_store;
 
 #[launch]
 fn rocket() -> _{
+    let mut config: Config;
+    #[cfg(debug_assertions)]
+    {
+        config = rocket::Config::debug_default();
+        config.port = 8080;
+    }
+    #[cfg(not(debug_assertions))]
+    {
+        config = rocket::Config::release_default();
+        config.port = 0;
+        config.log_level = rocket::log::LogLevel::Normal;
+        config.address = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
+    }
+
    rocket::build()
+    .configure(config)
     .mount("/", FileServer::from(relative!("web")))
     .mount("/api", routes![api::books, api::add_book])
 }
+
 
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize, Clone)]
 #[serde(crate="rocket::serde")]
