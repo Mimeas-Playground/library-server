b90eb39e0a3e3723e7d9bfc9760cd0c6e82b0e41
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2022-10-14 16:05:37 +0200

    Updated Frontend

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,25 +1,13 @@
-import { useEffect, useState } from 'react'
+<<<<<<< Updated upstream
+import { useState } from 'react'
+=======
+import { MouseEvent, useEffect, useState } from 'react'
+>>>>>>> Stashed changes
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  return (<>
-    <h1>SHHH!!!</h1>
-    <h2>This is a library</h2>
-    <hr/>
-    <BookList/>
-    </>)
-}
-
-function BookList() {
-  const [books, setBooks] = useState([]);
-  useEffect(() => {
-    fetch("/api")
-      .then(async (res) => {
-        setBooks(await res.json())
-      })
-  }, [])
-
+<<<<<<< Updated upstream
   return (
     <ul className="book">
       {
@@ -33,8 +21,71 @@ function BookList() {
   )
 }
 
+=======
+  return (<>
+    <h1>SHHH!!!</h1>
+    <h2>This is a library</h2>
+    <hr/>
+    <BookList/>
+    </>)
+}
+
+function BookList() {
+  const [books, setBooks] = useState([]);
+
+  function getBooks() {
+    console.log("I run!")
+
+    fetch("/api")
+      .then(async (res) => {
+        setBooks(await res.json())
+      })
+  }
+  useEffect(getBooks, [])
+
+  return (<>
+    <h3>Add a book</h3>
+    <BookForm update={()=>getBooks()}/>
+    <hr/>
+    <ul className="book">
+      {
+        books.map(
+          (b: Book) => {
+            return <li className='book'>{b.title}</li>
+          }
+        )
+      }
+    </ul>
+    </>)
+}
+
+function BookForm(props) {
+  const [title, setTitle] = useState('');
+
+  function submit(e: MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
+    fetch("/api", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({title})
+    }).finally(props.update)
+  }
+
+  return (
+    <form className="addBook">
+      <input type="text" placeholder="Title" onChange={(e) => setTitle(e.target.value)}/>
+      <button onClick={(e)=>submit(e)}>Add</button>
+    </form>
+  )
+}
+
+
+
 interface Book {
   title: string
 }
 
+>>>>>>> Stashed changes
 export default App
