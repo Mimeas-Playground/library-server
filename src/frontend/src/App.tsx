1cded086d605dd6695fb64be20ecab60113404e1
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2022-10-14 16:14:26 +0200

    Fixed unmerged changes

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,27 +1,8 @@
-<<<<<<< Updated upstream
-import { useState } from 'react'
-=======
 import { MouseEvent, useEffect, useState } from 'react'
->>>>>>> Stashed changes
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-<<<<<<< Updated upstream
-  return (
-    <ul className="book">
-      {
-        books.map(
-          (b: Book) => {
-            return <h3>{b.title}</h3>
-          }
-        )
-      }
-    </ul>
-  )
-}
-
-=======
   return (<>
     <h1>SHHH!!!</h1>
     <h2>This is a library</h2>
@@ -59,10 +40,10 @@ function BookList() {
     </>)
 }
 
-function BookForm(props) {
+function BookForm(props: { update: (() => void) | null | undefined; }) {
   const [title, setTitle] = useState('');
 
-  function submit(e: MouseEvent<HTMLButtonElement, MouseEvent>) {
+  function submit(e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) {
     e.preventDefault();
     fetch("/api", {
       method: "POST",
@@ -87,5 +68,4 @@ interface Book {
   title: string
 }
 
->>>>>>> Stashed changes
 export default App
