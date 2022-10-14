e1629ed8452c655015164b122d6b2e0644684f68
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2022-10-14 15:10:28 +0200

    💚 Should load from data storage

--- src/data_store.rs
+++ src/data_store.rs
@@ -1,9 +1,15 @@
+use std::{fs::File, path::PathBuf};
+
 use anyhow::Result;
+use rocket::{serde::json::serde_json, log::private::debug};
 use crate::Book;
 
 
 pub fn load() -> Result<Vec<Book>> {
-    unimplemented!()
+    let file = File::open(PathBuf::from("storage/db.json"))?;
+    debug!("Loading data from: {:?}", file);
+    let data = serde_json::from_reader(file)?;
+    Ok(data)
 }
 
 pub fn store() -> Result<()> {
