20a8e7a453e724d5094ae5dfec3a368bc1e4d10f
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2022-10-15 18:59:00 +0200

    💙 Made storage path a static variable

--- src/data_store.rs
+++ src/data_store.rs
@@ -3,9 +3,13 @@ use std::{fs::{File, create_dir_all}, path::PathBuf, io::{Write, Read}};
 use anyhow::Result;
 use crate::Book;
 
+lazy_static! {
+    static ref STORAGE_PATH: PathBuf = std::env::current_dir().unwrap().join("storage").join("db.json");
+}
+
 
 pub fn load() -> Result<Vec<Book>> {
-    let mut file = File::open(PathBuf::from("storage/db.json"))?;
+    let mut file = File::open(STORAGE_PATH.clone())?;
 
     let mut contents = vec![];
     file.read_to_end(&mut contents)?;
@@ -18,7 +22,7 @@ pub fn load() -> Result<Vec<Book>> {
 pub fn store(data: Vec<Book>) -> Result<()> {
     create_dir_all(PathBuf::from("storage"))?;
     
-    let mut file = File::create(PathBuf::from("storage/db.json"))?;
+    let mut file = File::create(STORAGE_PATH.clone())?;
     file.write_all(serde_json::ser::to_string(&data)?.as_bytes())?;
     file.write_all(b"\n")?;
 
